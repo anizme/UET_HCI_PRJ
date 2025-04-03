a4870a20299fc0ef88d20362e99f745337c608de
Ngô Duy Hiếu <hieu1804bg@gmail.com>
2025-04-03 17:12:53 +0700

    feat: api endpoints for vne news

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -7,6 +7,38 @@ const gTTS = require('gtts');
 const { createCanvas, loadImage } = require('canvas');
 const fs = require('fs').promises; // Thêm fs để xử lý file
 
+const rss_parser = require('rss-parser')
+const rssParser = new rss_parser()
+const cheerio = require('cheerio');
+// News list
+let cachedNewsList = []
+
+const newsTopics = [
+  "tin-moi-nhat",
+  "the-gioi",
+  "thoi-su",
+  "kinh-doanh",
+  "startup",
+  "giai-tri",
+  "the-thao",
+  "phap-luat",
+  "giao-duc",
+  "tin-moi-nhat",
+  "tin-noi-bat",
+  "suc-khoe",
+  "gia-dinh",
+  "du-lich",
+  "khoa-hoc",
+  "cong-nghe",
+  "oto-xe-may",
+  "y-kien",
+  "tam-su",
+  "cuoi",
+  "tin-xem-nhieu"
+]
+
+
+
 // API Object Recognition
 router.post('/object-recognition', async (req, res) => {
   try {
@@ -80,5 +112,43 @@ router.post('/tts', async (req, res) => {
     res.status(500).json({ error: 'Error generating audio', details: error.message });
   }
 });
+
+// Lấy tin tức từ vne, chuyển về danh sách các tin theo tên và index
+router.get('/fetch-news/:channel', async (req, res) => {
+  const channel = req.params.channel || 'tin-moi-nhat'
+  try {
+    const feed = await rssParser.parseURL(`https://vnexpress.net/rss/${channel}.rss`);
+    cachedNewsList = feed.items.map((item, index) => ({
+      index: index,
+      title: item.title,
+      link: item.link,
+      pubDate: item.pubDate,
+      description: item.contentSnippet,
+      channel: channel
+    }));
+    res.json({ success: true, news: cachedNewsList });
+  } catch (error) {
+    res.status(500).json({ success: false, message: 'Failed to fetch RSS feed', error });
+  }
+});
+
+router.get('/fetch-news/:channel/:newsIndex', async (req, res) => {
+  const newsIndex = req.params.newsIndex
+  try {
+    const currentNews = cachedNewsList[newsIndex]
+    const htmlNews = await fetch(currentNews.link).then(response => response.text())
+    
+
+    const $ = cheerio.load(htmlNews);
+
+    const parsedHTML = $('.fck_detail p').map((index, element) => `<p>${$(element).html()}</p>`).get();
+    const title = `<title_detail>${currentNews.title}<title_detail>`
+    const description = `<p>${currentNews.description}<p>`
+    const htmlContent = `${title}${description}${parsedHTML}`
+    res.json({ success: true, news: htmlContent})
+  } catch (error) {
+    res.status(500).json({ success: false, message: `Failed to fetch index ${newsIndex} from RSS feed`, error });
+  }
+})
 
 module.exports = router;
